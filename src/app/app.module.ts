a7457a95d524431440a76ba1935c378461974af7
Ruben Santibañez Acosta <rubensantibanezacosta902@gmail.com>
2021-09-27 18:58:32 +0100

    replace elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { ListComponent } from './menu/list/list.component';
-import { EntryComponent } from './menu/list/entry/entry.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ListComponent } from './components/list/list.component';
+import { EntryComponent } from './components/list/entry/entry.component';
 import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
